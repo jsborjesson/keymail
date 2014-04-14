1e9c8e5a6bfcbfd4e691a069b2619b3515397c06
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-14 16:08:01 +0200

    Screw fixtures, add context

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
 require "minitest/spec"
+require "factories"
 
 Turn.config.natural = true
 
@@ -13,7 +14,13 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+class Minitest::Spec
+  include ActiveSupport::Testing::Assertions # assert_difference etc...
+  include ActiveSupport::Testing::SetupAndTeardown # before, after
+
+  # Support rspec-style context-blocks
+  class << self
+    alias_method :context, :describe
+  end
+
 end
