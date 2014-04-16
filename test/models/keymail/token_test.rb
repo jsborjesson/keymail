8e345c5fb264844c9bae41cdde30ed8a434d8c3d
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-16 20:08:07 +0200

    Rewording token tests

--- test/models/keymail/token_test.rb
+++ test/models/keymail/token_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 module Keymail
   describe Token do
-    it 'generates a url_key' do
+    it 'generates a url_key when created' do
       token = Factory.create :token
       token.url_key.wont_be_nil
     end
 
-    it 'is invalid without an email' do
+    it 'must have an email' do
       token = Factory.build :token, email: ''
       token.must_have_invalid :email
     end
 
-    it 'has an expiration date' do
+    it 'must have an expiration date' do
       token = Factory.build :token, expires_at: nil
       token.must_have_invalid :expires_at
     end
