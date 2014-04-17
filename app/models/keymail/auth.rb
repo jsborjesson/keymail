29fe0f187b45a44985bd53b95a1440a4ae965013
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-17 16:36:43 +0200

    Auth module with tests

--- app/models/keymail/auth.rb
+++ app/models/keymail/auth.rb
@@ -1,8 +1,44 @@
 module Keymail
   module Auth
     def self.request(email)
+      # TODO: initializers for expires_at
       token = Token.create(email: 'whatever', expires_at: 10.minutes.since)
       AuthMailer.log_in(token).deliver
+    end
+
+    def self.verify_url_key(url_key)
+      token = Token.find_by(url_key: url_key)
+      token.destroy! unless token.nil?
+
+      return Failure.new if token.nil?
+      return Expired.new(token) if token.expired?
+
+      Success.new(token)
+    end
+  end
+
+  class TokenResponse
+    def initialize(token)
+      @token = token
+    end
+    delegate :email, :expired?, to: :@token
+  end
+
+  class Success < TokenResponse
+    def authenticated?
+      true
+    end
+  end
+
+  class Expired < TokenResponse
+    def authenticated?
+      false
+    end
+  end
+
+  class Failure
+    def authenticated?
+      false
     end
   end
 end
--- test/models/keymail/auth_test.rb
+++ test/models/keymail/auth_test.rb
@@ -12,5 +12,62 @@ module Keymail
         -> { Auth.request('test@email.com') }.must_change 'sent_emails.count', +1
       end
     end
+
+    context '.verify_url' do
+      it 'destroys the token' do
+        token = Factory :token
+        Auth.verify_url_key(token.url_key)
+        Token.exists?(token).must_equal false
+      end
+
+      it 'does not delete anything if the token is invalid' do
+        -> { Auth.verify_url_key('not_a_valid_key') }.wont_change 'Token.count'
+      end
+
+      context 'authenticated' do
+        let(:token) { Factory :token }
+        let(:response) { Auth.verify_url_key(token.url_key) }
+
+        it 'is authenticated' do
+          response.must_be :authenticated?
+        end
+
+        it 'has an email' do
+          response.email.must_equal token.email
+        end
+
+        it 'is not expired' do
+          response.wont_be :expired?
+        end
+
+        # first time, redirect...
+      end
+
+      context 'expired' do
+        let(:token) { Factory :token, expires_at: 10.minutes.ago }
+        let(:response) { Auth.verify_url_key(token.url_key) }
+
+        it 'is not authenticated' do
+          response.wont_be :authenticated?
+        end
+
+        it 'has an email' do
+          response.email.must_equal token.email
+        end
+
+        it 'is expired' do
+          response.must_be :expired?
+        end
+      end
+
+      context 'invalid' do
+        let(:response) { Auth.verify_url_key('invalid_url_key') }
+
+        it 'is not authenticated' do
+          response.wont_be :authenticated?
+        end
+      end
+
+    end
   end
 end
