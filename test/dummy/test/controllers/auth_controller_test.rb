e58915360a94f8735dd8fa58c6f201d021c1e5c1
Jimmy Börjesson <lagginglion@gmail.com>
2014-05-26 17:11:37 +0200

    Fix broken test and refactor

--- test/dummy/test/controllers/auth_controller_test.rb
+++ test/dummy/test/controllers/auth_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 describe AuthController do
 
+  let(:test_email) { 'test@email.com' }
+
   it 'renders the form' do
     get :new
     assert_template :new
@@ -13,16 +15,17 @@ describe AuthController do
   end
 
   it 'sends an email to the right address' do
-    test_email = 'test@email.com'
     post :request_email, email: test_email
+
     ActionMailer::Base.deliveries.wont_be :empty?
     ActionMailer::Base.deliveries.last.to.must_equal [test_email]
+
     assert_template :request_email
   end
 
   it 'redirects to success page with valid token' do
-    token = Factory :token
-    get :validate_link, url_key: token.url_key
+    get :validate_link, url_key: valid_token.url_key
+
     assert_redirected_to :success
   end
 
@@ -32,9 +35,16 @@ describe AuthController do
   end
 
   it 'redirects to error page with expired token' do
-    token = Factory :token, expires_at: 10.minutes.ago
-    get :validate_link, url_key: 'invalid_url_key'
+    get :validate_link, url_key: expired_token.url_key
     assert_redirected_to :fail
+  end
+
+  def expired_token
+    Factory :token, expires_at: 1.second.ago
+  end
+
+  def valid_token
+    Factory :token
   end
 
 end
